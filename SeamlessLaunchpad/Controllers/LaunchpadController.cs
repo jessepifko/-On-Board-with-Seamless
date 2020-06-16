c35faebfc057d8f0f5e65b89fbbc5e353c889dc5
millroy28 <millroy@gmail.com>
2020-06-15 23:25:49 -0400

    Added actions to add company interest, add to favorites, and view single

--- SeamlessLaunchpad/Controllers/LaunchpadController.cs
+++ SeamlessLaunchpad/Controllers/LaunchpadController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SeamlessLaunchpad.Models;
-
+using SeamlessLaunchpad.ViewModel;
 
 namespace SeamlessLaunchpad.Controllers
 {
@@ -53,9 +54,59 @@ namespace SeamlessLaunchpad.Controllers
         
         public IActionResult ViewDashboard()
         {
+            //Grabbing the User's seamless Association 
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var thisUser = _context.AspNetUsers.FirstOrDefault(x => x.Id == id);
+
+            //Sending list of startups and the favorites from DB and the user's association into the View
             var startups = _context.Startup.ToList();
-            return View(startups);
+            ViewStartupFavorite view = new ViewStartupFavorite() {
+                StartupsToReview = startups,
+                FavoriteStartups = _context.Favorites.Where(x => x.UserId == id).ToList<Favorites>(),
+                UserAssociation = thisUser.Association
+            };
+            return View(view);
         }
 
+        public IActionResult AddFavorite(int id)
+        {
+            Favorites newFavorite = new Favorites();
+            newFavorite.UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            newFavorite.StartupId = id;
+
+            if(ModelState.IsValid)
+            {
+                _context.Favorites.Add(newFavorite);
+                _context.SaveChanges();
+                return RedirectToAction("ViewDashboard");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult AddInterest(int id)
+        {
+            var startupToEdit = _context.Startup.Find(id);
+
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var thisUser = _context.AspNetUsers.FirstOrDefault(x => x.Id == userId);
+
+            startupToEdit.InterestedPartners += thisUser.Association;
+
+            _context.Entry(startupToEdit).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.Update(startupToEdit);
+            _context.SaveChanges();
+
+            return RedirectToAction("ViewDashboard");
+
+        }
+
+        public IActionResult ViewSingle(int id)
+        {
+            var startupToView = _context.Startup.Find(id);
+            return View(startupToView);
+        }
     }
 }
