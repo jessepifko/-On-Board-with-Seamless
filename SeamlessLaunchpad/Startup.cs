788ed6b01b383adc87410767412fed54ecf18637
Mike Reardon <homerun32295@gmail.com>
2020-06-24 16:42:29 -0400

    STYLES

--- SeamlessLaunchpad/Startup.cs
+++ SeamlessLaunchpad/Startup.cs
@@ -54,11 +54,9 @@ namespace SeamlessLaunchpad
             }
             else
             {
-                // app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
